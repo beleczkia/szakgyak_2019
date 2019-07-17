c166a009ac9e9f4d09597a0838d9c62bf392b074
LxKiss <36458304+LxKiss@users.noreply.github.com>
2019-07-17 16:56:10 +0200

    added summary and formatted folder structure

--- FWCO0F/src/Angular/src/app/services/odata.service.ts
+++ FWCO0F/src/Angular/src/app/services/odata.service.ts
@@ -4,10 +4,7 @@ import { HttpClient } from "@angular/common/http";
 @Injectable()
 export class OdataService {
   constructor(private http: HttpClient) { }
-
   configUrl = "/hana/odata.xsodata/";
-  //  Tarsasag?$top=1000&$format=json&$orderby=JEGYZ_TOKE_ERT_HUF+desc
-
   getData(params) {
     return this.http.get(this.configUrl + params);
   }
